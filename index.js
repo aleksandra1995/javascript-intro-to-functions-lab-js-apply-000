70098e4fbd86e401825c9f6a1867002795804a97
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 01:45:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
-  return "I can't hear you!"} {
-  if (string.toUppperCase() === string)
-  return "YES INDEED!"
+  return "I can't hear you!"} 
+  if {(string.toUppperCase() === string)
+  return "YES INDEED!"}
 }
  
